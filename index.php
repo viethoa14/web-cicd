dfbd3af48477675a6df37b862562b6ab8456029a
viethoa14 <58810284+viethoa14@users.noreply.github.com>
2022-10-22 22:16:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <title>CheckIndex</title>
    </head>
    <body>
-    <?php echo '<p>Hello world from php! G9 love_3</p>'; ?>
+    <?php echo '<p>Hello world from php! G9 love_4</p>'; ?>
    </body>
 </html>
     
